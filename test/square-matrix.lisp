02e97c4cd4337fe3f343595ed6437b8bb8ba5f3b
Thomas M. Hermann <thomas.m.hermann@odonata-research.com>
2013-01-27 23:03:13 -0600

    Unit test subtract and nsubtract for square matrices.

--- test/square-matrix.lisp
+++ test/square-matrix.lisp
@@ -1031,3 +1031,167 @@
          (15.5 16.0 16.5 17.0)
          (20.5 21.0 21.5 22.0))
      matrix1)))
+
+(define-test subtract-square-matrix
+  (:tag :square-matrix :subtract)
+  (let ((*epsilon* (* 3F0 single-float-epsilon))
+        (matrix1
+         (linear-algebra:make-matrix
+          4 4 :matrix-type 'linear-algebra:square-matrix
+          :initial-contents
+          #2A(( 2.2  2.4  2.6  2.8)
+              ( 4.2  4.4  4.6  4.8)
+              ( 6.2  6.4  6.6  6.8)
+              ( 8.2  8.4  8.6  8.8))))
+        (matrix2
+         (linear-algebra:make-matrix
+          4 4 :matrix-type 'linear-algebra:square-matrix
+          :initial-contents
+          #2A((1.1 1.2 1.3 1.4)
+              (2.1 2.2 2.3 2.4)
+              (3.1 3.2 3.3 3.4)
+              (4.1 4.2 4.3 4.4)))))
+    ;; No scalar
+    (assert-float-equal
+     #2A((1.1 1.2 1.3 1.4)
+         (2.1 2.2 2.3 2.4)
+         (3.1 3.2 3.3 3.4)
+         (4.1 4.2 4.3 4.4))
+     (linear-algebra:subtract matrix1 matrix2))
+    ;; Scalar1
+    (assert-float-equal
+     #2A(( 3.3  3.6  3.9  4.2)
+         ( 6.3  6.6  6.9  7.2)
+         ( 9.3  9.6  9.9 10.2)
+         (12.3 12.6 12.9 13.2))
+     (linear-algebra:subtract matrix1 matrix2 :scalar1 2.0))
+    ;; Scalar2
+    (assert-float-equal
+     #2A((0.0 0.0 0.0 0.0)
+         (0.0 0.0 0.0 0.0)
+         (0.0 0.0 0.0 0.0)
+         (0.0 0.0 0.0 0.0))
+     (linear-algebra:subtract matrix1 matrix2 :scalar2 2.0))
+    ;; Scalar1 & Scalar2
+    (assert-float-equal
+     #2A((1.1 1.2 1.3 1.4)
+         (2.1 2.2 2.3 2.4)
+         (3.1 3.2 3.3 3.4)
+         (4.1 4.2 4.3 4.4))
+     (linear-algebra:subtract
+      matrix1 matrix2 :scalar1 2.0 :scalar2 3.0))))
+
+(define-test nsubtract-square-matrix
+  (:tag :square-matrix :nsubtract)
+  ;; No scalar
+  (let ((matrix1
+         (linear-algebra:make-matrix
+          4 4 :matrix-type 'linear-algebra:square-matrix
+          :initial-contents
+          (make-array
+           '(4 4) :initial-contents
+           '(( 2.2  2.4  2.6  2.8)
+             ( 4.2  4.4  4.6  4.8)
+             ( 6.2  6.4  6.6  6.8)
+             ( 8.2  8.4  8.6  8.8)))))
+        (matrix2
+         (linear-algebra:make-matrix
+          4 4 :matrix-type 'linear-algebra:square-matrix
+          :initial-contents
+          #2A((1.1 1.2 1.3 1.4)
+              (2.1 2.2 2.3 2.4)
+              (3.1 3.2 3.3 3.4)
+              (4.1 4.2 4.3 4.4)))))
+    (assert-eq matrix1 (linear-algebra:nsubtract matrix1 matrix2))
+    (assert-float-equal
+     #2A((1.1 1.2 1.3 1.4)
+         (2.1 2.2 2.3 2.4)
+         (3.1 3.2 3.3 3.4)
+         (4.1 4.2 4.3 4.4))
+     matrix1))
+  ;; Scalar1
+  (let ((matrix1
+         (linear-algebra:make-matrix
+          4 4 :matrix-type 'linear-algebra:square-matrix
+          :initial-contents
+          (make-array
+           '(4 4) :initial-contents
+           '((1.1 1.2 1.3 1.4)
+             (2.1 2.2 2.3 2.4)
+             (3.1 3.2 3.3 3.4)
+             (4.1 4.2 4.3 4.4)))))
+        (matrix2
+         (linear-algebra:make-matrix
+          4 4 :initial-contents
+          #2A((1.1 1.2 1.3 1.4)
+              (2.1 2.2 2.3 2.4)
+              (3.1 3.2 3.3 3.4)
+              (4.1 4.2 4.3 4.4)))))
+    (assert-eq
+     matrix1
+     (linear-algebra:nsubtract matrix1 matrix2 :scalar1 2.0))
+    (assert-float-equal
+     #2A((1.1 1.2 1.3 1.4)
+         (2.1 2.2 2.3 2.4)
+         (3.1 3.2 3.3 3.4)
+         (4.1 4.2 4.3 4.4))
+     matrix1))
+  ;; Scalar2
+  (let ((*epsilon* (* 4F0 single-float-epsilon))
+        (matrix1
+         (linear-algebra:make-matrix
+          4 4 :matrix-type 'linear-algebra:square-matrix
+          :initial-contents
+          (make-array
+           '(4 4) :initial-contents
+           '(( 3.3  3.6  3.9  4.2)
+             ( 6.3  6.6  6.9  7.2)
+             ( 9.3  9.6  9.9 10.2)
+             (12.3 12.6 12.9 13.2)))))
+        (matrix2
+         (linear-algebra:make-matrix
+          4 4 :matrix-type 'linear-algebra:square-matrix
+          :initial-contents
+          #2A((1.1 1.2 1.3 1.4)
+              (2.1 2.2 2.3 2.4)
+              (3.1 3.2 3.3 3.4)
+              (4.1 4.2 4.3 4.4)))))
+    (assert-eq
+     matrix1
+     (linear-algebra:nsubtract matrix1 matrix2 :scalar2 2.0))
+    (assert-float-equal
+     #2A((1.1 1.2 1.3 1.4)
+         (2.1 2.2 2.3 2.4)
+         (3.1 3.2 3.3 3.4)
+         (4.1 4.2 4.3 4.4))
+     matrix1))
+  ;; Scalar1 & Scalar2
+  (let ((*epsilon* (* 3F0 single-float-epsilon))
+        (matrix1
+         (linear-algebra:make-matrix
+          4 4 :matrix-type 'linear-algebra:square-matrix
+          :initial-contents
+          (make-array
+           '(4 4) :initial-contents
+           '(( 2.2  2.4  2.6  2.8)
+             ( 4.2  4.4  4.6  4.8)
+             ( 6.2  6.4  6.6  6.8)
+             ( 8.2  8.4  8.6  8.8)))))
+        (matrix2
+         (linear-algebra:make-matrix
+          4 4 :matrix-type 'linear-algebra:square-matrix
+          :initial-contents
+          #2A((1.1 1.2 1.3 1.4)
+              (2.1 2.2 2.3 2.4)
+              (3.1 3.2 3.3 3.4)
+              (4.1 4.2 4.3 4.4)))))
+    (assert-eq
+     matrix1
+     (linear-algebra:nsubtract
+      matrix1 matrix2 :scalar1 2.0 :scalar2 3.0))
+    (assert-float-equal
+     #2A((1.1 1.2 1.3 1.4)
+         (2.1 2.2 2.3 2.4)
+         (3.1 3.2 3.3 3.4)
+         (4.1 4.2 4.3 4.4))
+     matrix1)))
