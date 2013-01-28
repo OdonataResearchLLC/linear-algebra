f856f0fbeb404078220a5db90c22fc49e7aad5fe
Thomas M. Hermann <thomas.m.hermann@odonata-research.com>
2013-01-27 18:36:52 -0600

    Formatting of the square matrix unit tests.

--- test/square-matrix.lisp
+++ test/square-matrix.lisp
@@ -38,117 +38,134 @@
 (define-test make-square-matrix
   (:tag :square-matrix :make-matrix)
   ;; A default square matrix
-  (let ((matrix (linear-algebra:make-matrix
-                 10 10
-                 :matrix-type 'linear-algebra:square-matrix)))
+  (let ((matrix
+         (linear-algebra:make-matrix
+          10 10 :matrix-type
+          'linear-algebra:square-matrix)))
     (assert-true (linear-algebra:matrixp matrix))
     (assert-true (typep matrix 'linear-algebra:square-matrix))
     (assert-rational-equal
      (make-array '(10 10) :initial-element 0)
      matrix))
   ;; Specify the square matrix element type
-  (let ((matrix (linear-algebra:make-matrix
-                 10 10
-                 :matrix-type 'linear-algebra:square-matrix
-                 :element-type 'single-float)))
+  (let ((matrix
+         (linear-algebra:make-matrix
+          10 10
+          :matrix-type 'linear-algebra:square-matrix
+          :element-type 'single-float)))
     (assert-true (linear-algebra:matrixp matrix))
     (assert-true (typep matrix 'linear-algebra:square-matrix))
-    (assert-eq (array-element-type
-                (linear-algebra::contents matrix))
-               (array-element-type
-                (make-array '(10 10) :element-type 'single-float)))
+    (assert-eq
+     (array-element-type
+      (linear-algebra::contents matrix))
+     (array-element-type
+      (make-array '(10 10) :element-type 'single-float)))
     (assert-float-equal
      (make-array '(10 10) :initial-element 0.0
                  :element-type 'single-float)
      matrix))
   ;; Specify the square matrix initial element
-  (let ((matrix (linear-algebra:make-matrix
-                 10 10
-                 :matrix-type 'linear-algebra:square-matrix
-                 :initial-element 1.0)))
+  (let ((matrix
+         (linear-algebra:make-matrix
+          10 10
+          :matrix-type 'linear-algebra:square-matrix
+          :initial-element 1.0)))
     (assert-true (linear-algebra:matrixp matrix))
     (assert-true (typep matrix 'linear-algebra:square-matrix))
     (assert-float-equal
      (make-array '(10 10) :initial-element 1.0)
      matrix))
   ;; Specify the square matrix contents - Nested list
-  (let* ((data '((1.1 1.2 1.3 1.4)
-                 (2.1 2.2 2.3 2.4)
-                 (3.1 3.2 3.3 3.4)
-                 (4.1 4.2 4.3 4.4))) 
-         (matrix (linear-algebra:make-matrix
-                  4 4
-                  :matrix-type 'linear-algebra:square-matrix
-                  :initial-contents data)))
+  (let* ((data
+          '((1.1 1.2 1.3 1.4)
+            (2.1 2.2 2.3 2.4)
+            (3.1 3.2 3.3 3.4)
+            (4.1 4.2 4.3 4.4))) 
+         (matrix
+          (linear-algebra:make-matrix
+           4 4
+           :matrix-type 'linear-algebra:square-matrix
+           :initial-contents data)))
     (assert-true (linear-algebra:matrixp matrix))
     (assert-true (typep matrix 'linear-algebra:square-matrix))
     (assert-float-equal
      (make-array '(4 4) :initial-contents data)
      matrix))
   ;; Specify the square matrix contents - Nested vector
-  (let* ((data #(#(1.1 1.2 1.3 1.4)
-                 #(2.1 2.2 2.3 2.4)
-                 #(3.1 3.2 3.3 3.4)
-                 #(4.1 4.2 4.3 4.4)))
-         (matrix (linear-algebra:make-matrix
-                  4 4
-                  :matrix-type 'linear-algebra:square-matrix
-                  :initial-contents data)))
+  (let* ((data
+          #(#(1.1 1.2 1.3 1.4)
+            #(2.1 2.2 2.3 2.4)
+            #(3.1 3.2 3.3 3.4)
+            #(4.1 4.2 4.3 4.4)))
+         (matrix
+          (linear-algebra:make-matrix
+           4 4
+           :matrix-type 'linear-algebra:square-matrix
+           :initial-contents data)))
     (assert-true (linear-algebra:matrixp matrix))
     (assert-true (typep matrix 'linear-algebra:square-matrix))
     (assert-float-equal
      (make-array '(4 4) :initial-contents data)
      matrix))
   ;; Specify the square matrix contents - 2D array
-  (let* ((data (make-array '(4 4) :initial-contents
-                           '((1.1 1.2 1.3 1.4)
-                             (2.1 2.2 2.3 2.4)
-                             (3.1 3.2 3.3 3.4)
-                             (4.1 4.2 4.3 4.4))))
-         (matrix (linear-algebra:make-matrix
-                  4 4
-                  :matrix-type 'linear-algebra:square-matrix
-                  :initial-contents data)))
+  (let* ((data
+          (make-array
+           '(4 4) :initial-contents
+           '((1.1 1.2 1.3 1.4)
+             (2.1 2.2 2.3 2.4)
+             (3.1 3.2 3.3 3.4)
+             (4.1 4.2 4.3 4.4))))
+         (matrix
+          (linear-algebra:make-matrix
+           4 4
+           :matrix-type 'linear-algebra:square-matrix
+           :initial-contents data)))
     (assert-true (linear-algebra:matrixp matrix))
     (assert-true (typep matrix 'linear-algebra:square-matrix))
     (assert-float-equal data matrix))
   ;; Erroneous 2D array input data
-  (assert-error 'error
-                (linear-algebra:make-matrix
-                 4 4
-                 :matrix-type 'linear-algebra:square-matrix
-                 :initial-contents
-                 #3A(((1.1 1.2) (2.1 2.2))
-                     ((3.1 3.2) (4.1 4.2))
-                     ((5.1 5.2) (6.1 6.2)))))
-  (assert-error 'error
-                (linear-algebra:make-matrix
-                 3 4
-                 :matrix-type 'linear-algebra:square-matrix
-                 :initial-contents
-                 (coordinate-array 0 0 3 4)))
-  (assert-error 'error
-                (linear-algebra:make-matrix
-                 4 3
-                 :matrix-type 'linear-algebra:square-matrix
-                 :initial-contents
-                 (coordinate-array 0 0 4 3)))
-  (assert-error 'error
-                (linear-algebra:make-matrix
-                 3 3 :element-type 'single-float
-                 :matrix-type 'linear-algebra:square-matrix
-                 :initial-contents
-                 #2A((1 2 3) (4 5 6) (7 8 9))))
+  (assert-error
+   'error
+   (linear-algebra:make-matrix
+    4 4
+    :matrix-type 'linear-algebra:square-matrix
+    :initial-contents
+    #3A(((1.1 1.2) (2.1 2.2))
+        ((3.1 3.2) (4.1 4.2))
+        ((5.1 5.2) (6.1 6.2)))))
+  (assert-error
+   'error
+   (linear-algebra:make-matrix
+    3 4
+    :matrix-type 'linear-algebra:square-matrix
+    :initial-contents
+    (coordinate-array 0 0 3 4)))
+  (assert-error
+   'error
+   (linear-algebra:make-matrix
+    4 3
+    :matrix-type 'linear-algebra:square-matrix
+    :initial-contents
+    (coordinate-array 0 0 4 3)))
+  (assert-error
+   'error
+   (linear-algebra:make-matrix
+    3 3 :element-type 'single-float
+    :matrix-type 'linear-algebra:square-matrix
+    :initial-contents
+    #2A((1 2 3) (4 5 6) (7 8 9))))
   ;; Specify initial element and initial contents
-  (assert-error 'error
-                (linear-algebra:make-matrix
-                 4 4
-                 :matrix-type 'linear-algebra:square-matrix
-                 :initial-element 1.1
-                 :initial-contents
-                 (coordinate-array 0 0 4 4))))
+  (assert-error
+   'error
+   (linear-algebra:make-matrix
+    4 4
+    :matrix-type 'linear-algebra:square-matrix
+    :initial-element 1.1
+    :initial-contents
+    (coordinate-array 0 0 4 4))))
 
 ;;; Test the square matrix predicate
+
 (define-test square-matrix-predicate
   (:tag :square-matrix)
   (assert-true
@@ -159,31 +176,37 @@
    (linear-algebra:square-matrix-p (make-array '(10 10)))))
 
 ;;; Test the square matrix bounds
+
 (define-test square-matrix-in-bounds-p
   (:tag :square-matrix :matrix-in-bounds-p)
   (test-matrix-in-bounds-p 'linear-algebra:square-matrix))
 
 ;;; Test the square matrix element type
+
 (define-test square-matrix-element-type
   (:tag :square-matrix :matrix-element-type)
   (test-matrix-element-type 'linear-algebra:square-matrix))
 
 ;;; Test the square matrix dimensions
+
 (define-test square-matrix-dimensions
   (:tag :square-matrix :matrix-dimensions)
   (test-matrix-dimensions 'linear-algebra:square-matrix 9 9))
 
 ;;; Test the square matrix row dimension
+
 (define-test square-matrix-row-dimension
   (:tag :square-matrix :matrix-row-dimension)
   (test-matrix-row-dimension 'linear-algebra:square-matrix 9 9))
 
 ;;; Test the square matrix column dimension
+
 (define-test square-matrix-column-dimension
   (:tag :square-matrix :matrix-column-dimension)
   (test-matrix-column-dimension 'linear-algebra:square-matrix 9 9))
 
 ;;; Reference square matrix elements
+
 (define-test square-matrix-mref
   (:tag :square-matrix :mref)
   (let* ((initial-contents
@@ -196,16 +219,18 @@
          (rend (1- rows)) (cend (1- columns))
          (rowi (random-interior-index rows))
          (coli (random-interior-index columns))
-         (data (make-array
-                (list rows columns)
-                :initial-contents
-                initial-contents))
-         (matrix (linear-algebra:make-matrix
-                  rows columns
-                  :matrix-type
-                  'linear-algebra:square-matrix
-                  :initial-contents
-                  initial-contents)))
+         (data
+          (make-array
+           (list rows columns)
+           :initial-contents
+           initial-contents))
+         (matrix
+          (linear-algebra:make-matrix
+           rows columns
+           :matrix-type
+           'linear-algebra:square-matrix
+           :initial-contents
+           initial-contents)))
     (assert-float-equal
      (aref data 0 0)
      (linear-algebra:mref matrix 0 0))
@@ -229,15 +254,16 @@
          (rend (1- rows)) (cend (1- columns))
          (rowi (random-interior-index rows))
          (coli (random-interior-index columns))
-         (matrix (linear-algebra:make-matrix
-                  rows columns
-                  :matrix-type 'linear-algebra:square-matrix
-                  :initial-contents
-                  '((1.1 1.2 1.3 1.4 1.5)
-                    (2.1 2.2 2.3 2.4 2.5)
-                    (3.1 3.2 3.3 3.4 3.5)
-                    (4.1 4.2 4.3 4.4 4.5)
-                    (5.1 5.2 5.3 5.4 5.5)))))
+         (matrix
+          (linear-algebra:make-matrix
+           rows columns
+           :matrix-type 'linear-algebra:square-matrix
+           :initial-contents
+           '((1.1 1.2 1.3 1.4 1.5)
+             (2.1 2.2 2.3 2.4 2.5)
+             (3.1 3.2 3.3 3.4 3.5)
+             (4.1 4.2 4.3 4.4 4.5)
+             (5.1 5.2 5.3 5.4 5.5)))))
     (destructuring-bind (val1 val2 val3 val4 val5)
         (make-random-list 5 1.0)
       (setf (linear-algebra:mref matrix 0 0)    val1)
@@ -245,41 +271,49 @@
       (setf (linear-algebra:mref matrix rend 0) val3)
       (setf (linear-algebra:mref matrix rend cend) val4)
       (setf (linear-algebra:mref matrix rowi coli) val5)
-      (assert-float-equal val1 (linear-algebra:mref matrix 0 0))
-      (assert-float-equal val2 (linear-algebra:mref matrix 0 cend))
-      (assert-float-equal val3 (linear-algebra:mref matrix rend 0))
-      (assert-float-equal val4 (linear-algebra:mref matrix rend cend))
-      (assert-float-equal val5 (linear-algebra:mref matrix rowi coli)))))
+      (assert-float-equal
+       val1 (linear-algebra:mref matrix 0 0))
+      (assert-float-equal
+       val2 (linear-algebra:mref matrix 0 cend))
+      (assert-float-equal
+       val3 (linear-algebra:mref matrix rend 0))
+      (assert-float-equal
+       val4 (linear-algebra:mref matrix rend cend))
+      (assert-float-equal
+       val5 (linear-algebra:mref matrix rowi coli)))))
 
 ;;; Copy the square matrix
 (define-test copy-square-matrix
   (:tag :square-matrix :copy-matrix)
-  (let ((matrix (linear-algebra:make-matrix
-                 5 5
-                 :matrix-type 'linear-algebra:square-matrix
-                 :initial-contents
-                 (coordinate-array 0 0 5 5))))
+  (let ((matrix
+         (linear-algebra:make-matrix
+          5 5
+          :matrix-type 'linear-algebra:square-matrix
+          :initial-contents
+          (coordinate-array 0 0 5 5))))
     (assert-true
      (linear-algebra:square-matrix-p
       (linear-algebra:copy-matrix matrix)))
     (assert-false
      (eq matrix (linear-algebra:copy-matrix matrix)))
     (assert-false
-     (eq (linear-algebra::contents matrix)
-         (linear-algebra::contents
-          (linear-algebra:copy-matrix matrix))))
+     (eq
+      (linear-algebra::contents matrix)
+      (linear-algebra::contents
+       (linear-algebra:copy-matrix matrix))))
     (assert-float-equal
      matrix (linear-algebra:copy-matrix matrix))))
 
 ;;; Test the submatrix of a square matrix
 (define-test square-submatrix
   (:tag :square-matrix :submatrix)
-  (let ((matrix (linear-algebra:make-matrix
-                 10 10
-                 :matrix-type
-                 'linear-algebra:square-matrix
-                 :initial-contents
-                 (coordinate-array))))
+  (let ((matrix
+         (linear-algebra:make-matrix
+          10 10
+          :matrix-type
+          'linear-algebra:square-matrix
+          :initial-contents
+          (coordinate-array))))
     ;; The entire matrix
     (assert-float-equal
      (coordinate-array)
@@ -291,11 +325,14 @@
     ;; End row and column
     (assert-float-equal
      (coordinate-array 3 4 5 5)
-     (linear-algebra:submatrix matrix 3 4 :end-row 5 :end-column 5))
+     (linear-algebra:submatrix
+      matrix 3 4 :end-row 5 :end-column 5))
     ;; Start row exceeds dimensions
-    (assert-error 'error (linear-algebra:submatrix matrix 11 5))
+    (assert-error
+     'error (linear-algebra:submatrix matrix 11 5))
     ;; Start column exceeds dimensions
-    (assert-error 'error (linear-algebra:submatrix matrix 5 11))
+    (assert-error
+     'error (linear-algebra:submatrix matrix 5 11))
     ;; End row exceeds dimensions
     (assert-error
      'error (linear-algebra:submatrix matrix 5 5 :end-row 11))
@@ -310,16 +347,18 @@
      'error (linear-algebra:submatrix matrix 7 7 :end-column 6))))
 
 ;;; Set the submatrix of a square matrix
+
 (define-test setf-square-submatrix
   (:tag :square-matrix :setf-submatrix)
   ;; Upper left submatrix
-  (let ((array-ul (make-array
-                   '(5 5) :initial-contents
-                   '((1 1 0 0 0)
-                     (1 1 0 0 0)
-                     (0 0 0 0 0)
-                     (0 0 0 0 0)
-                     (0 0 0 0 0)))))
+  (let ((array-ul
+         (make-array
+          '(5 5) :initial-contents
+          '((1 1 0 0 0)
+            (1 1 0 0 0)
+            (0 0 0 0 0)
+            (0 0 0 0 0)
+            (0 0 0 0 0)))))
     (assert-rational-equal
      array-ul
      (setf-submatrix
@@ -347,13 +386,14 @@
       (linear-algebra:submatrix
        (unit-matrix 5 5) 2 2 :end-row 4 :end-column 4))))
   ;; Upper right submatrix
-  (let ((array-ur (make-array
-                   '(5 5) :initial-contents
-                   '((0 0 0 1 1)
-                     (0 0 0 1 1)
-                     (0 0 0 0 0)
-                     (0 0 0 0 0)
-                     (0 0 0 0 0)))))
+  (let ((array-ur
+         (make-array
+          '(5 5) :initial-contents
+          '((0 0 0 1 1)
+            (0 0 0 1 1)
+            (0 0 0 0 0)
+            (0 0 0 0 0)
+            (0 0 0 0 0)))))
     (assert-rational-equal
      array-ur
      (setf-submatrix
@@ -381,13 +421,14 @@
       (linear-algebra:submatrix
        (unit-matrix 5 5) 2 2 :end-row 4 :end-column 4))))
   ;; Lower left submatrix
-  (let ((array-ll (make-array
-                   '(5 5) :initial-contents
-                   '((0 0 0 0 0)
-                     (0 0 0 0 0)
-                     (0 0 0 0 0)
-                     (1 1 0 0 0)
-                     (1 1 0 0 0)))))
+  (let ((array-ll
+         (make-array
+          '(5 5) :initial-contents
+          '((0 0 0 0 0)
+            (0 0 0 0 0)
+            (0 0 0 0 0)
+            (1 1 0 0 0)
+            (1 1 0 0 0)))))
     (assert-rational-equal
      array-ll
      (setf-submatrix
@@ -415,14 +456,15 @@
       (linear-algebra:submatrix
        (unit-matrix 5 5) 2 2 :end-row 4 :end-column 4))))
   ;; Lower right submatrix
-  (let ((array-lr (make-array
-                   '(5 5)
-                   :initial-contents
-                   '((0 0 0 0 0)
-                     (0 0 0 0 0)
-                     (0 0 0 0 0)
-                     (0 0 0 1 1)
-                     (0 0 0 1 1)))))
+  (let ((array-lr
+         (make-array
+          '(5 5)
+          :initial-contents
+          '((0 0 0 0 0)
+            (0 0 0 0 0)
+            (0 0 0 0 0)
+            (0 0 0 1 1)
+            (0 0 0 1 1)))))
     (assert-rational-equal
      array-lr
      (setf-submatrix
@@ -450,14 +492,15 @@
       (linear-algebra:submatrix
        (unit-matrix 5 5) 2 2 :end-row 4 :end-column 4))))
   ;; Middle submatrix
-  (let ((array-mid (make-array
-                    '(5 5)
-                    :initial-contents
-                    '((0 0 0 0 0)
-                      (0 1 1 1 0)
-                      (0 1 1 1 0)
-                      (0 1 1 1 0)
-                      (0 0 0 0 0)))))
+  (let ((array-mid
+         (make-array
+          '(5 5)
+          :initial-contents
+          '((0 0 0 0 0)
+            (0 1 1 1 0)
+            (0 1 1 1 0)
+            (0 1 1 1 0)
+            (0 0 0 0 0)))))
     (assert-rational-equal
      array-mid
      (setf-submatrix
@@ -495,14 +538,15 @@
     (zero-matrix 5 5 :matrix-type 'linear-algebra:square-matrix)
     (unit-matrix 5 5 :matrix-type 'linear-algebra:square-matrix)))
   ;; Replace the first 2 rows
-  (let ((result (make-array
-                 '(5 5)
-                 :initial-contents
-                 '((1 1 1 1 1)
-                   (1 1 1 1 1)
-                   (0 0 0 0 0)
-                   (0 0 0 0 0)
-                   (0 0 0 0 0)))))
+  (let ((result
+         (make-array
+          '(5 5)
+          :initial-contents
+          '((1 1 1 1 1)
+            (1 1 1 1 1)
+            (0 0 0 0 0)
+            (0 0 0 0 0)
+            (0 0 0 0 0)))))
     (assert-rational-equal
      result
      (linear-algebra:replace-matrix
@@ -532,14 +576,15 @@
       (unit-matrix 5 5 :matrix-type 'linear-algebra:square-matrix)
       :end-row2 2)))
   ;; Replace the first 3 columns
-  (let ((result (make-array
-                 '(5 5)
-                 :initial-contents
-                 '((1 1 1 0 0)
-                   (1 1 1 0 0)
-                   (1 1 1 0 0)
-                   (1 1 1 0 0)
-                   (1 1 1 0 0)))))
+  (let ((result
+         (make-array
+          '(5 5)
+          :initial-contents
+          '((1 1 1 0 0)
+            (1 1 1 0 0)
+            (1 1 1 0 0)
+            (1 1 1 0 0)
+            (1 1 1 0 0)))))
     (assert-rational-equal
      result
      (linear-algebra:replace-matrix
@@ -569,14 +614,15 @@
       (unit-matrix 5 5 :matrix-type 'linear-algebra:square-matrix)
       :end-column2 3)))
   ;; Replace the center
-  (let ((result (make-array
-                 '(5 5)
-                 :initial-contents
-                 '((0 0 0 0 0)
-                   (0 1 1 1 0)
-                   (0 1 1 1 0)
-                   (0 1 1 1 0)
-                   (0 0 0 0 0)))))
+  (let ((result
+         (make-array
+          '(5 5)
+          :initial-contents
+          '((0 0 0 0 0)
+            (0 1 1 1 0)
+            (0 1 1 1 0)
+            (0 1 1 1 0)
+            (0 0 0 0 0)))))
     (assert-rational-equal
      result
      (linear-algebra:replace-matrix
@@ -606,6 +652,7 @@
       :start-row2 2 :start-column2 2))))
 
 ;;; Validate a range for a square matrix.
+
 (define-test square-matrix-validated-range
   (:tag :square-matrix :matrix-validated-range)
   (test-matrix-validated-range
